8fbca4871ab605eb9d0b8474547885747ec8b004
Daniel Reeves <dreeves@beeminder.com>
2011-03-27 23:47:21 -0500

    new incarnation of bitch script

--- aux/bitch.sh
+++ aux/bitch.sh
@@ -3,5 +3,5 @@
 
 #echo -n "B: "; ../cntpings.pl bsoule.log  bsl hou -s2010.07.08
 #echo -n "D: "; ../cntpings.pl dreeves.log bsl hou -s2010.07.08
-echo -n "B: "; ../cntpings.pl ../bsoule.log  yc -s2010.07.08
-echo -n "D: "; ../cntpings.pl ../dreeves.log yc -s2010.07.08
+echo -n "B: "; ./cntpings.pl ./bsoule.log  yc -s2010.07.08
+echo -n "D: "; ./cntpings.pl ./dreeves.log yc -s2010.07.08
