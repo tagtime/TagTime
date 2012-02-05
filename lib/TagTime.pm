1bc59d1f33fab6b7670f8599c925f145f2ab159d
Paul Fenwick <pjf@perltraining.com.au>
2012-02-05 18:57:10 +1100

    Basic TagTime library.

---
+++ lib/TagTime.pm
@@ -0,0 +1,31 @@
+package TagTime;
+use strict;
+use warnings;
+
+=head2 match($expr, $line)
+
+Returns whether the boolean tag expression is true for the given line 
+from a log file (assume it's pre-stripped).
+
+=cut
+
+sub match {
+  my($expr, $line) = @_;
+  my %h;
+
+  return 1 if $expr =~ /^\s*\(?\s*\)?\s*$/;
+
+  for(split(/\s+/, $line)) { $h{$_} = 1; }
+
+  # TODO: Refactor this so we're not using eval() at the end.
+  # Based upon what the user has entered, almost anything
+  # could happen!
+
+  $expr =~ s/([^\|])\|([^\|])/$1\|\|$2/g;
+  $expr =~ s/([^\&])\&([^\&])/$1\&\&$2/g;
+  $expr =~ s/(\w+)/\$h{$1}/g;
+
+  return eval($expr);
+}
+
+1;
